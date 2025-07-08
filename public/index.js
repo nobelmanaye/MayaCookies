ead4a7014bf91fe1b7dc58faa8a577111bbe787c
Nobel Manaye <manayen23@mail.wlu.edu>
2025-07-08 18:03:55 -0400

    added a product.js to align with single functionality code design.

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@
 // OR if using npm:
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
-
+import { cookieMixes } from './products.js'; 
 document.addEventListener('DOMContentLoaded', function() {
   
 // Your web app's Firebase configuration
---
+++ public/products.js
@@ -0,0 +1,80 @@
+export const cookieMixes = 
+
+
+[
+ {
+        id: 'Cookie Single 1',
+        name: 'Bear Cookie',
+        image: 'Images/singles/final/bear_1.png',
+        price: 2.99,
+        quantity: 0,
+        type: 'single'
+    },
+    {
+        id: 'Cookie Single 2',
+        name: 'Hen Cookie',
+        image: 'Images/singles/final/hen_1.png',
+        price: 2.99,
+        quantity: 0,
+        type: 'single'
+    },
+    {
+        id: 'Cookie Single 4',
+        name: 'Pumpkin Cookie',
+        image: 'Images/singles/final/pumkpin.png',
+        price: 3.49,
+        quantity: 0,
+        type: 'single'
+    },
+    {
+        id: 'Cookie Single 5',
+        name: 'Sheep Cookie',
+        image: 'Images/singles/final/sheep_1.png',
+        price: 2.99,
+        quantity: 0,
+        type: 'single'
+    },
+    {
+        id: 'Cookie Single 6',
+        name: 'Snowman Cookie',
+        image: 'Images/singles/final/snowman.png',
+        price: 3.49,
+        quantity: 0,
+        type: 'single'
+    },
+  
+,
+    {
+        id: 'Cookie Mix 1',
+        name: 'Assorted Chocolate Mix',
+        image: 'Images/new mixes/mix_1.png',
+        price: 12.99,
+        quantity: 0,
+        type: 'mix'
+    },
+    {
+        id: 'Cookie Mix 2',
+        name: 'Holiday Special Mix',
+        image: 'Images/new mixes/mix_2.png',
+        price: 14.99,
+        quantity: 0,
+        type: 'mix'
+    },
+    {
+        id: 'Cookie Mix 3',
+        name: 'Classic Vanilla Mix',
+        image: 'Images/new mixes/mix_3.png',
+        price: 11.99,
+        quantity: 0,
+        type: 'mix'
+    },
+    {
+        id: 'Cookie Mix 4',
+        name: 'Premium Nut Mix',
+        image: 'Images/new mixes/mix_4.png',
+        price: 15.99,
+        quantity: 0,
+        type: 'mix'
+    }
+]
+
